9af5322a8339f3cd70a9ab9da0f219c6c03e98d0
Aly <waly@mgc.mentorg.com>
2022-09-12 11:07:00 +0100

    add new dir with write mode

---
+++ lib1/hdl/uart_top_rtl.vhd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:45c55f0d2a1504a19b2447823d5353410df478b2a7789062e81da82885c45cc3
+size 6309
---
+++ lib2/hdl/cpu_interface_rtl.vhd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5d83a67b82504bc01308363341bbffcf0e0615ce6ac9124664f3f448e253ca00
+size 2091
